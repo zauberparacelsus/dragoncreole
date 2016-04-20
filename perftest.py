aa8d2387a823b0090eda553db31eebd980d3ee60
Zauber Paracelsus <zauber@gridmail.org>
2016-04-20 14:13:10 -0400

    Moved perftest.py back to the root directory

--- perftest.py
+++ perftest.py
@@ -7,7 +7,7 @@ from dragoncreole import DragonCreole
 parser = DragonCreole()
 
 def DoTest(i):
-	with open("test.txt", "r") as f:
+	with open("dragoncreole/test.txt", "r") as f:
 		text = f.read()
 	i = int(i)
 	for x in range(i):
